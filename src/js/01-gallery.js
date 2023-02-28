42d64f6e87e08a97100f7849794d41e91b55bcb3
Okazaki92 <vincentslominski@gmail.com>
2023-02-28 21:23:48 +0100

    Update tsk 1 and 2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,25 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+const galleryItem = galleryItems
+	.map(
+		({ preview, original, description }) =>
+			`<a class="gallery__item" href="${original}">
+            <img class="gallery__image" 
+            src="${preview}" 
+            alt="${description}"/>
+            </a>`,
+	)
+	.join("");
+const mainGallery = document.querySelector(".gallery");
 
+mainGallery.insertAdjacentHTML("beforeend", galleryItem);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+	captions: true,
+	captionsData: "alt",
+	captionDelay: 250,
+});
 console.log(galleryItems);
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,15 @@
+import Player from "@vimeo/player";
+import throttle from "lodash.throttle";
+import "../css/common.css";
 
+const iframe = document.querySelector("#vimeo-player");
+const player = new Player(iframe);
+const TIME_KEY = "videoplayer-current-time";
+
+const onPlay = function (data) {
+	localStorage.setItem(TIME_KEY, data.seconds);
+};
+
+player.on("timeupdate", throttle(onPlay, 1000));
+
+player.setCurrentTime(JSON.parse(localStorage.getItem(TIME_KEY)) || 0);
